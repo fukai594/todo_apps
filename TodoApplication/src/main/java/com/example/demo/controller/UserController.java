360ea95570a2b07cd74b7333ca43f08cc3ac91e2
mayu <@em.com>
2025-06-24 09:54:21 +0900

    不要なクラス削除

--- TodoApplication/src/main/java/com/example/demo/controller/UserController.java
+++ TodoApplication/src/main/java/com/example/demo/controller/UserController.java
@@ -1,20 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.demo.form.UserForm;
 
 @Controller
 public class UserController {
 	
-	@GetMapping("/home")
-	public String home(Model model) {
-	 	UserForm userForm = new UserForm();
-	 	model.addAttribute("userForm", userForm);
-		return "/home";
-	}
 	@GetMapping("/login")
 	public String login() {
         return "/login";
